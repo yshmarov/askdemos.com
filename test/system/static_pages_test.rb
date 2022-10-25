18878bd0b62e93a56feff40654a74511b2daa819
yshmarov <yshmarov@gmail.com>
2022-10-26 01:22:41 +0200

    add basic system test

---
+++ test/system/static_pages_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class StaticPagesTest < ApplicationSystemTestCase
+  test "visiting the index" do
+    visit root_url
+  
+    assert_selector "h1", text: "landing_page"
+  end
+end
