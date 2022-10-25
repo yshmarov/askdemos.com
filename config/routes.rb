e44323030a0cd6c124b2f53d56e7e20a3bce243d
yshmarov <yshmarov@gmail.com>
2022-10-26 01:51:24 +0200

    install devise for User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'pricing', to: 'static_pages#pricing'
   get 'terms', to: 'static_pages#terms'
   get 'privacy', to: 'static_pages#privacy'
