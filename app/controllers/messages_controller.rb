4a5d1da74ccc22a016c3b4da976d497fd5a6cd26
yshmarov <yshmarov@gmail.com>
2022-10-26 23:54:57 +0200

    lint

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,61 +10,47 @@ class MessagesController < ApplicationController
   def show
   end
 
-  # GET /messages/new
   def new
     @message = Message.new
   end
 
-  # GET /messages/1/edit
   def edit
   end
 
-  # POST /messages or /messages.json
   def create
     @message = Message.new(message_params)
-
     respond_to do |format|
       if @message.save
-        format.html { redirect_to message_url(@message), notice: 'Message was successfully created.' }
-        format.json { render :show, status: :created, location: @message }
+        format.html { redirect_to message_url(@message), notice: t('.notice') }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /messages/1 or /messages/1.json
   def update
     respond_to do |format|
       if @message.update(message_params)
-        format.html { redirect_to message_url(@message), notice: 'Message was successfully updated.' }
-        format.json { render :show, status: :ok, location: @message }
+        format.html { redirect_to message_url(@message), notice: t('.notice') }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /messages/1 or /messages/1.json
   def destroy
     @message.destroy
-
     respond_to do |format|
-      format.html { redirect_to messages_url, notice: 'Message was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to messages_url, notice: t('.notice') }
     end
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_message
     @message = Message.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def message_params
     params.require(:message).permit(:inbox_id, :user_id, :body, :status)
   end
