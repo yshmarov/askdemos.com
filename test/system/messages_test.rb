8c0e82150a1c4654258580e31d172f6efe553a8e
yshmarov <yshmarov@gmail.com>
2022-10-27 10:06:33 +0200

    make system tests work

--- test/system/messages_test.rb
+++ test/system/messages_test.rb
@@ -5,44 +5,28 @@ class MessagesTest < ApplicationSystemTestCase
 
   setup do
     sign_in users(:one)
+    @inbox = inboxes(:one)
     @message = messages(:one)
   end
 
   test 'visiting the index' do
-    visit messages_url
+    visit inbox_messages_url(@inbox)
     assert_selector 'h1', text: 'Messages'
   end
 
   test 'should create message' do
-    visit messages_url
+    visit inbox_messages_url(@inbox)
     click_on 'New message'
 
     fill_in 'Body', with: @message.body
-    fill_in 'Inbox', with: @message.inbox_id
-    fill_in 'Status', with: @message.status
-    fill_in 'User', with: @message.user_id
     click_on 'Create Message'
 
     assert_text 'Message was successfully created'
-    click_on 'Back'
-  end
-
-  test 'should update Message' do
-    visit message_url(@message)
-    click_on 'Edit this message', match: :first
-
-    fill_in 'Body', with: @message.body
-    fill_in 'Inbox', with: @message.inbox_id
-    fill_in 'Status', with: @message.status
-    fill_in 'User', with: @message.user_id
-    click_on 'Update Message'
-
-    assert_text 'Message was successfully updated'
-    click_on 'Back'
+    click_on 'New message'
   end
 
   test 'should destroy Message' do
-    visit message_url(@message)
+    visit inbox_messages_url(@inbox)
     click_on 'Destroy this message', match: :first
 
     assert_text 'Message was successfully destroyed'
