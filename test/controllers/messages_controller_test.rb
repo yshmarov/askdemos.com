02b6a69fbea324863ee9aa5c7cfffe5199ce837b
yshmarov <yshmarov@gmail.com>
2022-10-27 09:57:59 +0200

    Update messages_controller_test.rb

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -5,49 +5,34 @@ class MessagesControllerTest < ActionDispatch::IntegrationTest
 
   setup do
     sign_in users(:one)
+    @inbox = inboxes(:one)
     @message = messages(:one)
   end
 
   test 'should get index' do
-    get messages_url
+    get inbox_messages_url(@inbox)
     assert_response :success
   end
 
   test 'should get new' do
-    get new_message_url
+    get new_inbox_message_url(@inbox)
     assert_response :success
   end
 
   test 'should create message' do
     assert_difference('Message.count') do
-      post messages_url,
+      post inbox_messages_url(@inbox),
            params: { message: { body: @message.body, inbox_id: @message.inbox_id, status: @message.status, user_id: @message.user_id } }
     end
 
-    assert_redirected_to message_url(Message.last)
-  end
-
-  test 'should show message' do
-    get message_url(@message)
-    assert_response :success
-  end
-
-  test 'should get edit' do
-    get edit_message_url(@message)
-    assert_response :success
-  end
-
-  test 'should update message' do
-    patch message_url(@message),
-          params: { message: { body: @message.body, inbox_id: @message.inbox_id, status: @message.status, user_id: @message.user_id } }
-    assert_redirected_to message_url(@message)
+    assert_redirected_to inbox_messages_url(@inbox)
   end
 
   test 'should destroy message' do
     assert_difference('Message.count', -1) do
-      delete message_url(@message)
+      delete inbox_message_url(@message.inbox, @message)
     end
 
-    assert_redirected_to messages_url
+    assert_redirected_to inbox_messages_url(@inbox)
   end
 end
